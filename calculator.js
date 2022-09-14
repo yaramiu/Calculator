26005bb9f8ba969dd89e871cfc9043d02a8243c5
Brian Liao <yaramiu@gmail.com>
2022-09-13 20:40:26 -0400

    Add files and implement operate function

---
+++ calculator.js
@@ -0,0 +1,38 @@
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+  let result = 0;
+
+  switch (operator) {
+    case "+":
+      result = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      result = subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      result = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      result = divide(firstNumber, secondNumber);
+      break;
+    default:
+      result = null;
+  }
+
+  return result;
+}
